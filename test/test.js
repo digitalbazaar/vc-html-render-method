5157e087c12fa1fda315b2bfd6802a8173ae4343
Parth Bhatt <pbjm2493@gmail.com>
2026-08-04 07:33:29 -0700

    Add karma test harness.

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/*!
+ * Copyright (c) 2026 Digital Bazaar, Inc.
+ */
+import * as bedrock from '@bedrock/core';
+import '@bedrock/https-agent';
+import '@bedrock/express';
+import '@bedrock/server';
+import '@bedrock/test';
+import '@bedrock/karma';
+
+bedrock.start();
